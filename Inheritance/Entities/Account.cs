aa10f6377a01bdbb06334e9ee44b4d2e0b98d2b3
Igork <igorkw@live.com>
2024-03-12 15:41:44 -0300

    Another little example of inheritance

---
+++ Inheritance/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿namespace Heranca.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; } //private só pode sera alterado pela própria classe
+        public double Balance { get; protected set; } //protected: o saldo só pode ser alterado pela classe ou subclasse
+
+        public Account() 
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Heranca.Entities;
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount acount = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(acount.Balance); //é possível acessar o valor do saldo
+
+            //acount.Balance = 200.0; //não é possível alterar o saldo (pois a prop está como private)
+        }
+    }
+}
